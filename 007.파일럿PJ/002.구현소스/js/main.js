b157ac6c5c8acb7a7618d00c98bbc95e316a9660
SRNOSE <90014573+SRNOSE@users.noreply.github.com>
2021-10-25 11:04:02 +0900

    ss

--- 007.파일럿PJ/002.구현소스/js/main.js
+++ 007.파일럿PJ/002.구현소스/js/main.js
@@ -51,9 +51,17 @@ $(function(){//////////////////////////JQB///////////////////////////////
     //현재 슬라이드 위치값 구하기
     //슬라이드 위치값
     let spos;
+    //이징변수
+    let easing = "easeOutQuint";
+    //화면커버(광드래그막기)
+    let cover = $(".cover");
 
     //이벤트 :dragstop-> 드래그가 끝날때
    slide.on("dragstop", function(){
+
+    //광드래그막기 커버보이기
+    cover.show();
+    
      //슬라이드 위치값 구하기
      spos = slide.offset().left;
      //offset().left화면 왼쪽 기준선 left위치
@@ -67,9 +75,22 @@ $(function(){//////////////////////////JQB///////////////////////////////
      if(spos < -win*1.1){
 
       //슬라이드가 -200% 위치로 이동한다
-      slide.animate({
+      //stop()매서드는 animate가 큐에 쌓이는 것을 막는다!
+      slide.stop().animate({
         left: -win*2+"px"
-      }, 700);/////////animate///////////////
+      }, 600,easing, function(){//콜백함수(이동후)
+        //첫번째 슬라이드li를 맨 뒤로 보내기
+        //변경대상:slide
+        slide
+        .append(slide.find("li").first())
+        //이때 left값을 -100%위치로 고정해야함
+        .css({
+          left:-win+"px"});
+
+          //광드래그 커버 지우기
+          cover.hide();
+
+      });/////////animate콜백함수///////////////
       
 
      }/////////////if문 : -110%보다 작을때////////////////////
@@ -81,9 +102,20 @@ $(function(){//////////////////////////JQB///////////////////////////////
       else if(spos > -win*0.9){
 
       //슬라이드가 0위치로 이동한다
-      slide.animate({
+      slide.stop().animate({
         left: "0px"
-      }, 700);/////////animate///////////////
+      }, 600,easing,function(){//콜백함수(이동후)
+        //대상: .slide-> slide변수
+        slide
+        // 맨뒤의 슬라이드 li를 맨앞으로 이동
+        .prepend(slide.find("li").last())
+        //left값을 원래 위치인 -100%로 변경
+         .css({
+           left:-win+"px"});
+          //광드래그 커버 지우기
+          cover.hide();
+
+      });/////////animate콜백함수///////////////
       
 
      }/////////////else if문 : -90%보다 클때////////////////////
@@ -92,9 +124,14 @@ $(function(){//////////////////////////JQB///////////////////////////////
      //-110% < 범위 < -90%
      else{
         //슬라이드가 원위치로 돌아온다
-      slide.animate({
+      slide.stop().animate({
         left: -win+"px"
-      }, 300);/////////animate///////////////
+      }, 300,easing,function(){//콜백함수(이동후)
+         //광드래그 커버 지우기
+         cover.hide();
+
+
+      });/////////animate///////////////
 
      }/////else문: 사이범위///////////
 
