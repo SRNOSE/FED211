4defc37869b3ffb5b8155c277be921bdf2de54fa
SRNOSE <90014573+SRNOSE@users.noreply.github.com>
2021-10-25 14:06:50 +0900

    ss

---
+++ 009.Ajax학습/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>첫페이지</title>
+  <!--php코딩영역 -->
+  <?php
+  //주석
+  /* 주석 */
+  #주석
+
+  // php변수는 문자앞에 $를 씀
+  $link = "<a href ='vogue/'>보그바로가기</a> ";
+    //화면출력->echo메시지
+  echo "<h1>나의 첫 PHP페이지!</h1>";
+  
+  echo "<h2>$link</h2>";
+  
+  ?>
+</head>
+<body>
+  
+</body>
+</html>
