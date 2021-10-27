bb28b8cfe32a55c91601daaf35dd5a74ab857d52
SRNOSE <90014573+SRNOSE@users.noreply.github.com>
2021-10-27 16:29:40 +0900

    ss

--- 007.파일럿PJ/002.구현소스/js/men.js
+++ 007.파일럿PJ/002.구현소스/js/men.js
@@ -98,10 +98,30 @@ $(function () { /// jQB ////////////////////////////
 
 
   /// 부드러운 스크롤 호출!(제이쿼리 아님!)
+  startSS();
 
   /////////////////////////////////////
   // 배너에 스와이퍼 플러그인 적용하기 /// 
   /////////////////////////////////////
+  var swiper = new Swiper(".mySwiper", {
+
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+  }); /////swiper///////////////////////////////
 
 
   /// 스크롤 등장 플러그인 적용 : 스크롤리빌 ////
@@ -112,7 +132,7 @@ $(function () { /// jQB ////////////////////////////
   ////////////////////////////////////////////////
   //////////////// 신상품 움직이기 ////////////////
   ////////////////////////////////////////////////
-  
+
 
 
 
